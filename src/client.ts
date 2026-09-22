94cf0f4bd1e1bc06550bb194af778ba34649f6d0
shpaw415 <shpaw415@gmail.com>
2026-09-22 00:29:14 -0400

    up

--- src/client.ts
+++ src/client.ts
@@ -1,1 +1,9 @@
 const remoteVersion = await fetch("/api/versionTest").then((res) => res.text());
+
+const localVersion = localStorage.getItem("CF_PAGES_CURRENT_VERSION");
+
+if (remoteVersion !== localVersion) {
+	localStorage.setItem("CF_PAGES_CURRENT_VERSION", remoteVersion);
+	await fetch("/api/versionTest", { method: "DELETE" });
+	location.reload();
+}
